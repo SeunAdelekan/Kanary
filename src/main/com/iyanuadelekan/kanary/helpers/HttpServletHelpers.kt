2ae7a1a166f6b192bbfed326e360f33079c39f79
Iyanu Adelekan <iyanuadelekan@gmail.com>
2017-05-27 22:57:25 +0100

    Added HttpServletHelpers.kt

---
+++ src/main/com/iyanuadelekan/kanary/helpers/HttpServletHelpers.kt
@@ -0,0 +1,17 @@
+package com.iyanuadelekan.kanary.helpers
+
+import javax.servlet.http.HttpServletRequest
+
+/**
+ * @author Iyanu Adelekan on 27/05/2017.
+ */
+
+/**
+ * Helper function for the easy retrieval
+ * of HTTP request body content.
+ *
+ * This function is added to the receiver [HttpServletRequest]
+ */
+fun HttpServletRequest.getBody() {
+
+}
