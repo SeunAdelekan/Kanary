2fe83fdc1577715a94ddb3df988dda2b0ea15b7c
Iyanu Adelekan <iyanuadelekan@gmail.com>
2017-05-27 23:03:14 +0100

    Added helper functions to HttpServletHelpers

--- src/main/com/iyanuadelekan/kanary/helpers/HttpServletHelpers.kt
+++ src/main/com/iyanuadelekan/kanary/helpers/HttpServletHelpers.kt
@@ -1,5 +1,6 @@
 package com.iyanuadelekan.kanary.helpers
 
+import java.util.stream.Collectors
 import javax.servlet.http.HttpServletRequest
 
 /**
@@ -7,11 +8,20 @@ import javax.servlet.http.HttpServletRequest
  */
 
 /**
- * Helper function for the easy retrieval
+ * [HttpServletRequest] helper function for the easy retrieval
  * of HTTP request body content.
  *
  * This function is added to the receiver [HttpServletRequest]
  */
 fun HttpServletRequest.getBody() {
+    val requestJSONBody = reader.lines().collect(Collectors.joining(System.lineSeparator()))
+}
 
+/**
+ * [HttpServletRequest] helper function for the easy retrieval
+ * of HTTP request body content as a JSON string.
+ * @return string containing JSON body
+ */
+fun HttpServletRequest.getBodyAsJson(): String {
+    return reader.lines().collect(Collectors.joining(System.lineSeparator()))
 }
