6e09ce6d2ae46519734239ca21ce3a4acb9cfc2f
Iyanu Adelekan <iyanu.adelekan@konga.com>
2018-08-15 17:00:54 +0100

    Started work on database and mailer service adapters.

---
+++ src/main/com/iyanuadelekan/kanary/component/adapter/mail/MailerServiceAdapter.kt
@@ -0,0 +1,40 @@
+package com.iyanuadelekan.kanary.component.adapter.mail
+
+/**
+ * Interface to be extended by email service providers
+ * to facilitate the sending of mails in Kanary applications via their service.
+ */
+interface MailerServiceAdapter {
+
+    /**
+     * @property clientId - Unique identifier of client. Often generated
+     * upon registration with an email service provider. Kindly contact your
+     * service provider for help retrieving this.
+     */
+    val clientId: String
+
+    /**
+     * @property clientSecret - Client secret of service user. This is generally
+     * generated upon registration with an email service provider. Please contact
+     * your service provider to request for one.
+     */
+    val clientSecret: String
+
+    /**
+     * This method upon implementation will be invoked to send
+     * emails to receivers via a compatible mailer service.
+     *
+     * @param title - The email title.
+     * @param subject - The email subject.
+     * @param body - The body of the email.
+     * @param sender - The sender of the email.
+     * @param receiver - The email receiver.
+     * @return [Any] - Service provider response.
+     */
+    fun sendMail(
+            title: String,
+            subject: String,
+            body: String,
+            sender: String,
+            receiver: String): Any
+}
